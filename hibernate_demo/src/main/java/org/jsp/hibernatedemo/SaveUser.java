1822ba58f21b2272ab6b14e19109f7a5ad0b7d9f
josep <josephgeo13@gmail.com>
2023-08-17 15:19:51 +0530

    1.Created a new maven project 2.Added dependencies in pom.xml 3.Configured hibernate.cfg.xml 4.created Test class in org.jsp.hibernate package 5.Created an entity class 6.Configured hibernate mapping file(user.hbm.xml) 7.Created a class SaveUser to insert a record

---
+++ hibernate_demo/src/main/java/org/jsp/hibernatedemo/SaveUser.java
@@ -0,0 +1,23 @@
+package org.jsp.hibernatedemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class SaveUser {
+	public static void main(String[] args){
+		User u= new User();
+		u.setEmail("abc@gmail.com");
+		u.setName("ABC");
+		u.setPhone(7778888);
+		u.setPassword("abc123");
+		Configuration cfg=new Configuration().configure();
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session s=factory.openSession();
+		int id=(Integer) s.save(u);
+		Transaction t=s.beginTransaction();
+		t.commit();
+	}
+
+}
